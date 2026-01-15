21c94d576ad10c2018d49d2beb3761738e55ea01
phwnganh <phuonganh25102003@gmail.com>
2026-01-15 23:04:47 +0700

    fix: layout shift of project title

--- src/components/MyTasksComponent/MyTaskTitle.tsx
+++ src/components/MyTasksComponent/MyTaskTitle.tsx
@@ -37,9 +37,9 @@ const MyTaskTitle = ({projectId}: {projectId: string}) => {
     }
 
     return (
-        <div className={"relative"} >
+        <div className={"relative min-h-12"} >
             <h1 className={`absolute inset-0 hover:outline hover:outline-product-library-border-hover-tint ${commonClass} ${!isEditing ? "opacity-100" : "opacity-0"}`} onClick={() => setEditing(true)}>{title}</h1>
-            <textarea ref={textareaRef} className={`${commonClass} resize-none outline-product-library-border-focus-tint
+            <textarea ref={textareaRef} className={`absolute inset-0 ${commonClass} resize-none outline-product-library-border-focus-tint
     border-none bg-transparent ${isEditing ? "opacity-100" : "opacity-0 pointer-events-none"}`} spellCheck={false} value={title} maxLength={120} onChange={e => setTitle(e.target.value)} onBlur={handleBlurTitle}></textarea>
         </div>
 
