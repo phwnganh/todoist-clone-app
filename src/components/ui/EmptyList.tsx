604757c494104ea14a2caacab3523fe164da3851
phwnganh <phuonganh25102003@gmail.com>
2026-01-15 10:33:30 +0700

    fix: the main content > sidebar content - not affect sidebar content by auto stretching

--- src/components/ui/EmptyList.tsx
+++ src/components/ui/EmptyList.tsx
@@ -2,7 +2,7 @@ import EmptyImage from '../../assets/not-found-list.avif'
 const EmptyList = () => {
     return (
         <div className={"flex flex-col justify-center items-center"}>
-            <img src={EmptyImage} className={"empty-image"}/>
+            <img src={EmptyImage} alt={"empty-image"}/>
             <p className={"text-xl font-medium"}>No Results Found!</p>
         </div>
     );
