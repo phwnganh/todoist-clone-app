13623adc7331da857f632750d3ace45cb66b36bb
phwnganh <phuonganh25102003@gmail.com>
2026-03-13 11:13:52 +0700

    fix: handle drag drop in inbox page and set id to add section/task

--- src/pages/InboxPage/index.tsx
+++ src/pages/InboxPage/index.tsx
@@ -11,23 +11,33 @@ import ThreeDotsIcon from "@/components/icons/ThreeDotsIcon.tsx";
 import {queryClient} from "@/main.tsx";
 import MyTasksList from "@/components/MyTasksComponent/MyTasksList.tsx";
 import MyTasksBoard from "@/components/MyTasksComponent/MyTasksBoard.tsx";
-import {useGetAllSections} from "@/hooks/useQueryHook/useSections.ts";
+import {useGetAllSections, useReorderSection} from "@/hooks/useQueryHook/useSections.ts";
 import {useGroupingTaskStore} from "@/stores/groupingTask.store.ts";
 import type {Section} from "@/types/section.type.ts";
 import {useGetAllProjects} from "@/hooks/useQueryHook/useProjects.ts";
+import {useDragStore} from "@/stores/dragDrop.store.ts";
+import {DndContext, type DragEndEvent, type DragOverEvent} from "@dnd-kit/core";
+import {findSectionByIdToOrder, handleReorderSection} from "@/helpers/dragDropMySection.ts";
+import {useMoveMyTask, useReorderTask} from "@/hooks/useQueryHook/useTasks.ts";
+import {findTaskByIdToOrder, handleReorderTask} from "@/helpers/dragDropMyTasks.ts";
+import {customCollisionDetection} from "@/helpers/customCollisionDetection.ts";
+import {useProjectStore} from "@/stores/project.store.ts";
 
 const InboxPage = () => {
     const {showCollapse, onToggleSidebar} = useOutletContext<HeaderLayoutType>()
     const {groupedBy, setGroupedBy} = useGroupingTaskStore()
     const [openLayoutDropdown, setOpenLayoutDropdown] = useState(false);
     const {mutate: updateViewOptions} = useViewOptions()
+    const {mutateAsync: movingTaskMutate} = useMoveMyTask()
+    const {mutateAsync: reorderingTaskMutate} = useReorderTask()
+    const {mutate: reorderingSectionMutate} = useReorderSection()
     const isGrouping = groupedBy != null;
     const {data: projects} = useGetAllProjects()
     const inboxProjectId = useMemo(() => projects?.results?.find(p => p.name === "Inbox")?.id, [projects?.results])
     console.log("inbox id", inboxProjectId)
     const {data: sections} = useGetAllSections({project_id: inboxProjectId});
     const {data: allTasks, isLoading} = useTasksWithView({project_id: inboxProjectId}, "PROJECT", inboxProjectId);
-
+    const setProjectId = useProjectStore(state => state.setProjectId)
     const viewOptions = queryClient.getQueryData<ViewOptionsPayload>(["viewOptions", "PROJECT", inboxProjectId])
 
 
@@ -38,8 +48,6 @@ const InboxPage = () => {
             ...payload,
         });
     };
-
-
 
     useEffect(() => {
         setGroupedBy(viewOptions?.grouped_by ?? null)
@@ -60,11 +68,58 @@ const InboxPage = () => {
         return allTasks?.grouped ?? []
     }, [isGrouping, allTasks?.grouped])
 
+    useEffect(() => {
+        if(inboxProjectId){
+            setProjectId(inboxProjectId)
+        }
+    }, [inboxProjectId, setProjectId])
+
     const NO_SECTION = {
         id: null,
         name: "(No section)",
         project_id: inboxProjectId,
     } as Section;
+
+    const {setDragPreview, clearDragPreview} = useDragStore()
+    const handleDragEnd = async (e: DragEndEvent) => {
+        const {active, over} = e
+        if(!over || active.id === over.id) return;
+        const activeType = active.data.current?.type;
+        const overType = over.data.current?.type;
+        const dropType = over.data.current?.dropType;
+        const isDropInside = overType === "task" && activeType === "task" && dropType === "inside";
+        if(activeType === "section" && overType === "section"){
+            const sectionList = sections?.results;
+            if(!sectionList) return;
+
+            const activeSection = findSectionByIdToOrder(sectionList, active.id as string)
+            const overSection = findSectionByIdToOrder(sectionList, over.id as string)
+
+            if(!activeSection || !overSection) return;
+            handleReorderSection(sectionList, activeSection, overSection, reorderingSectionMutate)
+        }
+        const tasks = allTasks?.results
+        if(!tasks) return;
+
+        const activeTask = findTaskByIdToOrder(tasks, active.id as string)
+        const overTask = findTaskByIdToOrder(tasks, over.id as string)
+        if(!activeTask || !overTask) return;
+
+        await handleReorderTask(tasks, activeTask, overTask, inboxProjectId, movingTaskMutate, reorderingTaskMutate, isDropInside)
+        clearDragPreview()
+    }
+
+    const handleDragOver = (e: DragOverEvent) => {
+        const {active, over} = e
+        if(!over) return;
+        const dropType = over.data.current?.dropType ?? null;
+
+        setDragPreview({
+            activeId: active.id as string,
+            overId: over.id as string,
+            dropType
+        })
+    }
     return (
         <>
             <HeaderLayout showCollapse={showCollapse} onToggleSidebar={onToggleSidebar} right={
@@ -88,25 +143,28 @@ const InboxPage = () => {
                 </div>
             }></HeaderLayout>
 
-            {layoutName === "LIST" ? (
-                <section className={"max-w-200 mx-auto w-full relative z-10"}>
-                    <div className={"flex flex-col gap-small"}>
-                        <h1 className="p-1 font-strong text-product-library-display-primary-idle-tint text-header-large">
-                            Inbox
-                        </h1>
-                        <MyTasksList filteredSectionsByProject={sections?.results} isGrouping={isGrouping} groupedTasks={groupedTasks} tasks={allTasks?.results ?? []} noSection={NO_SECTION} isLoading={isLoading}/>
-                    </div>
-                </section>
-            ) : (
-                <section className={"px-10"}>
-                    <div className={"flex flex-col gap-small"}>
-                        <h1 className="p-1 font-strong text-product-library-display-primary-idle-tint text-header-large">
-                            Inbox
-                        </h1>
-                        <MyTasksBoard filteredSectionsByProject={sections?.results} isGrouping={isGrouping} groupedTasks={groupedTasks} tasks={allTasks?.results ?? []} noSection={NO_SECTION} isLoading={isLoading}/>
-                    </div>
-                </section>
-            )}
+            <DndContext onDragEnd={handleDragEnd} onDragOver={handleDragOver} collisionDetection={customCollisionDetection}>
+                {layoutName === "LIST" ? (
+                    <section className={"max-w-200 mx-auto w-full relative z-10"}>
+                        <div className={"flex flex-col gap-small"}>
+                            <h1 className="p-1 font-strong text-product-library-display-primary-idle-tint text-header-large">
+                                Inbox
+                            </h1>
+                            <MyTasksList filteredSectionsByProject={sections?.results} isGrouping={isGrouping} groupedTasks={groupedTasks} tasks={allTasks?.results ?? []} noSection={NO_SECTION} isLoading={isLoading}/>
+                        </div>
+                    </section>
+                ) : (
+                    <section className={"px-10"}>
+                        <div className={"flex flex-col gap-small"}>
+                            <h1 className="p-1 font-strong text-product-library-display-primary-idle-tint text-header-large">
+                                Inbox
+                            </h1>
+                            <MyTasksBoard filteredSectionsByProject={sections?.results} isGrouping={isGrouping} groupedTasks={groupedTasks} tasks={allTasks?.results ?? []} noSection={NO_SECTION} isLoading={isLoading}/>
+                        </div>
+                    </section>
+                )}
+            </DndContext>
+
         </>
     );
 };
